f95001f0aa8fffd63738d68ad01171c718558362
Adrian <adrianvalverde45@gmail.com>
2023-10-31 15:40:35 -0400

    commit tree f372884d05aa452ad1ffd8a10ced979b9f76a4a2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait?
+
+    CLICKBAIT = ["Won't Believe", "Secret", "Top", "Guess"]
+
+    def title_is_clickbait?
+        byebug
+        if title
+            if CLICKBAIT.none? {|pat| title.include?(pat)}
+                errors.add(:title, "Title must be more clickbait-y")
+            end
+        end
+    end
+
 end
